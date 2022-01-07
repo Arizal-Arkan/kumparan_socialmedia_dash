6de9900811048ae408c551cd29641a9274f3f723
ArizaArkan <arizalarkan@gmail.com>
2022-01-07 22:31:30 +0700

    slicing design to code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
 
 function Home() {
     return (
         <div>
-            <p style={{ color: 'red' }}>Lol</p>
+            <h1>What on your mind ?</h1>
+            <div style={{ width: '70%', backgroundColor: '#2b2f38', height: '300px', borderRadius: '30px', margin: '0 auto' }}>
+                <TextField />
+            </div>
         </div>
     )
 }
